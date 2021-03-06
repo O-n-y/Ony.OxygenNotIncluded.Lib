ff079ca2ce428951c2531274bc7906ff094720c4
O-n-y <O-n-y@users.noreply.github.com>
2021-03-06 11:43:17 +0300

    Added Assembly Extensions

---
+++ Ony.OxygenNotIncluded.Lib/Extensions/Assembly_Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Ony.OxygenNotIncluded.Lib.Extensions
+{
+	public static class Assembly_Extensions
+	{
+		public static Type[] GetLoadableTypes(this Assembly assembly)
+		{
+			if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+			try
+			{
+				return assembly.GetTypes();
+			}
+			catch (ReflectionTypeLoadException e)
+			{
+				return e.Types.Where(t => t != null).ToArray();
+			}
+		}
+
+		public static IEnumerable<Type> GetTypesWithAttribute<T>(this Assembly assembly) => assembly.GetLoadableTypes().Where(type => type.GetCustomAttributes(typeof(T), true).Length > 0);
+
+	}
+}
