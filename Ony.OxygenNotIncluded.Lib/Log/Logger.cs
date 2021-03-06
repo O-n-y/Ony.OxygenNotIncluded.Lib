3c4f649a3c4abbd7839e25e52586897befc6c146
O-n-y <O-n-y@users.noreply.github.com>
2021-03-06 12:02:46 +0300

    Added Logger

---
+++ Ony.OxygenNotIncluded.Lib/Log/Logger.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using UnityEngine;
+
+namespace Ony.OxygenNotIncluded.Lib
+{
+	public class Logger
+	{
+		private static string _prefix;
+		private static bool   _isConsoleEnabled;
+
+		public static void Init(string prefix)
+		{
+			_prefix           = prefix;
+			_isConsoleEnabled = true;
+		}
+
+		private static string Prefix => $"[{_prefix}][{Thread.CurrentThread.ManagedThreadId}]";
+		public static void Print(string message)
+		{
+			if(_isConsoleEnabled) return;
+
+			Console.WriteLine($"\x1b[48;5;23m  {Prefix}: {message} \x1b[48;5;0m");
+			
+		}
+		public static void Print(Component component)
+		{
+			if(_isConsoleEnabled) return;
+
+			Console.WriteLine($"\x1b[48;5;23m  {Prefix}: {component.name}: {component.GetType()}  \x1b[48;5;0m");
+		}
+
+		public static void Header(string message)
+		{
+			if(_isConsoleEnabled) return;
+			Console.WriteLine($"\x1b[48;5;24m  {Prefix}: {message} \x1b[48;5;0m");
+			
+		}
+
+		public static void Start(string message)
+		{
+			if (Console.Out.GetType().ToString() == "UnityEngine.UnityLogWriter")
+			{
+				Console.WriteLine($"{Prefix}: {message}");
+				return;
+			}
+
+			Console.WriteLine($"\x1b[48;5;21m  {Prefix}: {message} \x1b[48;5;0m");
+			
+		}
+
+		public static void Print(Exception message)
+		{
+			if(_isConsoleEnabled) return;
+
+			Console.WriteLine($"\x1b[48;5;1m  {Prefix}: {message} \x1b[48;5;0m");
+		}
+
+
+		public static void PrintError(string msg, string stackTrace)
+		{
+			if(_isConsoleEnabled) return;
+
+			Console.WriteLine($"\x1b[48;5;1m  {Prefix}: {msg}\n{stackTrace} \x1b[48;5;0m");
+		}
+
+		public static void PrintError(string msg)
+		{
+			if(_isConsoleEnabled) return;
+
+			var stackFrames = Environment.StackTrace.Split('\n').Skip(1).ToArray();
+			var stackTrace  = string.Join("\n", stackFrames);
+
+			Console.WriteLine($"\x1b[48;5;1m  {Prefix}: {msg}\n{stackTrace} \x1b[48;5;0m");
+		}
+
+		public static void PrintAction(string msg)
+		{
+			if(_isConsoleEnabled) return;
+			
+			Console.WriteLine($"\x1b[48;5;3m  {Prefix}: {msg} \x1b[48;5;0m");
+		}
+	
+	}
+
+}
